8ab1d6d36b9b9cdac4d35c781886d79b33f2d277
jankikumar <85163886+jankikumar@users.noreply.github.com>
2021-11-17 21:37:01 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,10 +3,10 @@ class Hello
 public static void main (String as [])
 {
   System.out.println("welcome to jlc");
-  System.out.println("welcome to jlc");
-  System.out.println("welcome to jlc");
-  System.out.println("welcome to jlc");
-  System.out.println("welcome to jlc");
+  System.out.println("welcome to gaya");
+  System.out.println("welcome to j");
+  System.out.println("welcome to manpur");
+  System.out.println("welcome to loowa");
   
 }
 }
